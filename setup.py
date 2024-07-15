57d729a24879b8b42f7aa49df81e2d68c9ed956a
Malik Al Morhiby <124366776+malace01@users.noreply.github.com>
2024-07-15 16:36:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='docker-to-podman',
-    version='1.1',  # Updated version
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         'flask',
-        'pyyaml',  # Added dependency
+        'pyyaml',
+        'argcomplete',
     ],
     entry_points={
         'console_scripts': [
-            'docker-to-podman=docker_to_podman.translator:run_command',
+            'docker-to-podman=docker_to_podman.cli:main',
         ],
     },
 )
